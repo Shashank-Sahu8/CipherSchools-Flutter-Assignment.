db5b7dc743019efecb43d22d2c8df4f87586bcd8
Shashank-Sahu8 <shashanksahu87070@gmail.com>
2024-01-16 18:28:14 +0530

    Shashank Sahu1

--- lib/Pages/Budget.dart
+++ lib/Pages/Budget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Budget extends StatefulWidget {
   const Budget({super.key});
@@ -10,6 +11,26 @@ class Budget extends StatefulWidget {
 class _BudgetState extends State<Budget> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/pending.png',
+              height: 200,
+            ),
+            Text(
+              'Budget section will be updated soon',
+              style: GoogleFonts.inter(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
